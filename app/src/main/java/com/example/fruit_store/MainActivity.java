3866a5261266a127d70071ad7c3437d93647bf4c
lamPham3107 <pham31lam07@gmail.com>
2025-02-28 16:02:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fruit_store/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.fruit_store;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fruit_store.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void login(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void registration(View view) {
+        startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+    }
+}
