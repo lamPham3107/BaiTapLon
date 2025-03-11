da526f16bf592c9115ac09ba5df648a8a7d3f31d
lamPham3107 <pham31lam07@gmail.com>
2025-03-11 07:38:51 +0700

    .

--- app/src/main/java/com/example/fruit_store/models/FruitModel.java
+++ app/src/main/java/com/example/fruit_store/models/FruitModel.java
@@ -7,15 +7,23 @@ public class FruitModel implements Serializable {
     private String price;
     private String img_url;
     private String description;
-
+    private String quantity;
     public FruitModel() {
     }
 
-    public FruitModel(String name, String price, String img_url, String description) {
+    public FruitModel(String name, String price, String img_url, String description, String quantity) {
         this.name = name;
         this.price = price;
         this.img_url = img_url;
         this.description = description;
+        this.quantity = quantity;
+    }
+
+    public FruitModel(String description, String img_url, String name, String price) {
+        this.description = description;
+        this.img_url = img_url;
+        this.name = name;
+        this.price = price;
     }
 
     public String getDescription() {
@@ -26,14 +34,6 @@ public class FruitModel implements Serializable {
         this.description = description;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getImg_url() {
         return img_url;
     }
@@ -42,11 +42,27 @@ public class FruitModel implements Serializable {
         this.img_url = img_url;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPrice() {
         return price;
     }
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
     }
 }
